5ec1267eb4e432bd961d537b156cfbf9991493a8
unknown <brightgems@live.cn>
2020-02-07 19:24:04 +0800

    max_steps_without_decrease

--- bert_base/train/train_helper.py
+++ bert_base/train/train_helper.py
@@ -74,6 +74,7 @@ def get_args_parser():
     group2.add_argument('-crf_only', action='store_true', default=False)
     group2.add_argument('-lstm_only', action='store_true', default=False)
     group2.add_argument('-finetune', action='store_true', default=False)
+    group2.add_argument('-max_steps_without_decrease',type=int, default=500)
     group2.add_argument('-device_map', type=str, default='0',
                         help='witch device using to train')
 
